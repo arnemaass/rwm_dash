549705b4749fac515244828a1f524e2ad8445d08
arnemaass <arne.maa@gmx.de>
2024-09-26 15:57:55 +0200

    add preprocessing script

---
+++ rwm_preprocessing.R
@@ -0,0 +1,90 @@
+# RWM Preprocessing
+
+rm(list = ls())
+setwd("Z:/Shared/Shiny/2024/rwm_dash")
+
+library(tidyverse)
+library(sf)
+library(arrow)
+
+rwm<- readRDS("data/data_kreise.rds") # already merged with district shapefiles
+
+
+##  Create a complete geometry grid for all years
+rwm1 <- rwm %>%
+  group_by(year, geometry) %>%
+  summarise(participants=sum(participants, na.rm = TRUE)+1,
+            demo_count= n()+1) %>%
+  ungroup()
+
+# get the cross product of years and geometry, add kreise
+YG_grid <- crossing(year = unique(rwm$year), geometry = rwm$geometry) %>% left_join(distinct(select(rwm, geometry, kreis)), by = "geometry")
+
+
+rwm1 <- left_join(YG_grid, rwm1, by = c("year", "geometry"))%>%
+  replace_na(list(participants = 1, demo_count = 1)) %>%
+  st_as_sf()
+
+saveRDS(rwm1, "data/rwm_shiny.rds")
+
+
+## Aggregate the participants and demonstration count by year and federal state
+
+
+# Generate all combinations of yearmonth and state
+all_combinations <- rwm %>% 
+  st_set_geometry(NULL) %>%
+  select(year, state) %>%
+  distinct() %>%
+  complete(year = unique(rwm$year), state = unique(rwm$state)) %>% 
+  na.omit()
+
+# Aggregate the data
+rwm2 <- rwm  %>%
+  st_set_geometry(NULL) %>%
+  group_by(year, state) %>%
+  summarise(participants = sum(participants, na.rm = TRUE),
+            demo_count = n()) %>%
+  ungroup()  
+
+
+# Merge with all_combinations to retain missing months/states
+rwm2 <- all_combinations %>%
+  left_join(rwm2, by = c("year", "state")) %>%
+  mutate(participants = ifelse(is.na(participants), 0, participants),
+         demo_count = ifelse(is.na(demo_count), 0, demo_count),
+         year = substr(year, 1, 4))
+
+
+write_parquet(rwm2, "data/rwm_stateyear.parquet")
+
+
+## Aggregate the participants and demonstration count by month and federal state
+
+
+# Generate all combinations of yearmonth and state
+all_combinations <- rwm %>% 
+  st_set_geometry(NULL) %>%
+  select(yearmonth, state) %>%
+  distinct() %>%
+  complete(yearmonth = unique(rwm$yearmonth), state = unique(rwm$state)) %>% 
+  na.omit()
+
+# Aggregate the data
+rwm3 <- rwm  %>%
+  st_set_geometry(NULL) %>%
+  group_by(yearmonth, state) %>%
+  summarise(participants = sum(participants, na.rm = TRUE),
+            demo_count = n()) %>%
+  ungroup()  
+
+
+# Merge with all_combinations to retain missing months/states
+rwm3 <- all_combinations %>%
+  left_join(rwm2, by = c("yearmonth", "state")) %>%
+  mutate(participants = ifelse(is.na(participants), 0, participants),
+         demo_count = ifelse(is.na(demo_count), 0, demo_count),
+         yearmonth = substr(yearmonth, 1, 4))
+
+
+write_parquet(rwm3, "data/rwm_statemonth.parquet")
